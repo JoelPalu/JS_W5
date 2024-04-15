daa920a7780ea5ada8d369be0439f809dfca301d
JoelPalu <kirill.saveliev@metropolia.fi>
2024-04-15 14:00:05 +0300

    add dialog

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,7 +42,6 @@ const mediaArray = [
 
 const Home = () => {
   const [selectedItem, setSelectedItem] = useState(mediaArray[0]);
-
   return (
     <>
       <h2>My Media</h2>
@@ -64,7 +63,7 @@ const Home = () => {
           ))}
         </tbody>
       </table>
-      <SingleView selectedItem={selectedItem}></SingleView>
+      <SingleView selectedItem={selectedItem} setSelectedItem={setSelectedItem}></SingleView>
     </>
   );
 };
